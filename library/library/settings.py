fbd2afcd8fee0d3d880b1bbb925a51ea19185c70
Carlita L Abbé <carl@Carlitas-MacBook-Pro.local>
2018-10-13 17:28:13 -0400

    got it running

--- library/library/settings.py
+++ library/library/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'library.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'soen341',
+        'NAME': 'sys',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'admin12345',
         'HOST': 'localhost',
         'PORT': '3306',
     }
