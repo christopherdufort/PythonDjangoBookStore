10068c8451f4726e7ab1f6eb4fbad3abb71936cf
Alessandro <kalessandro14@gmail.com>
2018-10-13 11:54:46 -0400

    javascript linkage

---
+++ library/static/js/sign-in.js
@@ -0,0 +1,12 @@
+var hello = "hello javascript world"
+
+document.getElementById("demo").innerHTML = hello;
+
+function signIn(email, password){
+    //something here
+    //document.getElementById('textbox_id').value
+}
+
+function createAccount(email, password){
+
+}
