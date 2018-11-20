9672762ff67db92be00d7fb924f253e8aaae81d8
surya64 <suryaprakash.govindaraju@gmail.com>
2018-11-19 22:01:05 -0500

    Detail view of each item in the catalog

--- LibraryCatalog/library/settings.py
+++ LibraryCatalog/library/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'soen341',
         'USER': 'root',
-        'PASSWORD': '1234',
+        'PASSWORD': 'Admin123',
         'HOST': 'localhost',
         'PORT': '3306',
     }
