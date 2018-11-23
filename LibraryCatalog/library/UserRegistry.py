5cfa5660f870a8ada06763b5b803c7bed226cc81
shruthikvenkat <shruthi.kv20@gmail.com>
2018-11-23 02:41:14 -0500

    Naming conventions as per SAD and SRS

--- LibraryCatalog/library/UserRegistry.py
+++ LibraryCatalog/library/UserRegistry.py
@@ -49,7 +49,7 @@ class UserRegistry:
         return f_user
 
 
-    def get_active_users():
+    def viewLoggedUsers(self):
         #session_key exists 
         #session_expire > today's date
         today = datetime.datetime.today()
